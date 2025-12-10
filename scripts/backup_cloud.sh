53efce789f956f806bc6d1205828c76c9216de6d
serg333soul <serg333soul@gmail.com>
2025-12-10 07:50:58 +0000

    Fix7: Final fix for ShellCheck SC2086 in backup script

--- scripts/backup_cloud.sh
+++ scripts/backup_cloud.sh
@@ -62,7 +62,7 @@ docker exec "$DB_CONTAINER" mariadb-dump -u "$DB_USER" -p "$DB_PASS" nextcloud |
 if [ ${PIPESTATUS[0]} -eq 0 ]; then
     log "SUCCESS | Дамп створено."
     # FIX SC2086: Лапки додано тут
-    $RCLONE_BIN --config "$RCLONE_CONFIG" copy "$PATH_DB_DUMP/nextcloud_$TIMESTAMP.sql.gz" "$RCLONE_REMOTE/Database"
+    "$RCLONE_BIN" --config "$RCLONE_CONFIG" copy "$PATH_DB_DUMP/nextcloud_$TIMESTAMP.sql.gz" "$RCLONE_REMOTE/Database"
     find "$PATH_DB_DUMP" -name "*.sql.gz" -mtime +7 -delete
 else
     log "ERROR | Помилка дампа бази!"
